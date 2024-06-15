a32dacc6c19d1b25c7fd9c21f467e6b13abaf58a
Vivek Kumar Singh <vivek.inboxme@gmail.com>
2024-06-16 00:50:57 +0530

    updated  empty img

--- src/components/Empty.jsx
+++ src/components/Empty.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { logo } from "../assets";
 
 function Empty() {
   return (
     <div className='bg-white h-[100vh] flex flex-col items-center'>
       <div className='flex flex-col justify-center items-center pt-28'>
-        <img src="https://cdn.zeptonow.com/app/images/empty-bag.png?tr=w-640,q-70" alt="emptycart" className='h-[50px] mb-5' />
+        <img src={logo} alt="emptycart" className='h-[50px] mb-5' />
         <p>Your cart is empty</p>
         <a href="/"><button className='border-2 border-[#FF3269] text-[#FF3269] mt-3 rounded px-4 py-1'>Browse Products</button></a>
         </div>
